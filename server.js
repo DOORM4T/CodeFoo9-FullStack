1036013b8876a2c59fb0d62f6d2ca0f379f2720f
Matt Seto <46092255+DOORM4T@users.noreply.github.com>
2019-03-10 22:04:32 -0700

    Add user name functionality.

--- server.js
+++ server.js
@@ -7,20 +7,30 @@ app.use(express.static(`${__dirname}/public`));
 let port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
 
+
+// Usernames
+
 // Initialize Socket.io
 const io = require('socket.io')(server);
 
 // On client connect
 io.on('connection', (socket) => {
   let id = socket.id;
-  console.log(`(ID:${id}) User Connected...`)
+  console.log(`(ID:${id}) User connected...`)
+
 
   // Handle user disconnect
   socket.on('disconnect', () => console.log(`(ID:${id}) User disconnected...`));
 
   // Handle chat message sending
-  socket.on('new message', msg => {
+  socket.on('new message', data => {
     // Tell all clients to add message.
-    io.emit('get message', msg);
+    io.emit('get message', data);
+  });
+  
+  // Broadcasting new user entering chat
+  socket.on('new user',user=>{
+    io.emit('announce user',user);
   });
 });
+
