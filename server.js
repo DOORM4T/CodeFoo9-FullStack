115180a08b63396a5c04e3a82aef676857ccb0c6
Matt Seto <46092255+DOORM4T@users.noreply.github.com>
2019-03-15 23:29:45 -0700

    Connect app to MongoDB Atlas.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Create Express app
+// Create Express Server
 const express = require('express')
 const app = express();
 
@@ -7,31 +7,64 @@ app.use(express.static(`${__dirname}/public`));
 let port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
 
-// Initialize Socket.io
-const io = require('socket.io')(server);
-let username;
 
-// On client connect
-io.on('connection', (socket) => {
-  let id = socket.id;
-  console.log(`(ID:${id}) User connected...`)
+// Connect to MongoDB Atlas
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://door:matt@codefoo9-chatapp-jahed.azure.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  // Initialize Socket.io
+  const io = require('socket.io')(server);
 
-  // Broadcasting new user entering chat
-  socket.on('new user',user=>{
-    username=user;
-    io.emit('announce user',username);
-  });
+  // On client connect
+  io.on('connection', (socket) => {
+    // Connect DB
+    let collection = client.db("global-chat").collection("messages");
+    let username;
 
-  // Handle user disconnect
-  socket.on('disconnect', () =>{ 
-    console.log(`(ID:${id}) User disconnected...`)
-    io.emit('user disconnected',username);
-  });
+    // Add alert to DB
+    const addAlert = msg => {
+      collection.insertOne({message:msg}, ()=>{
+        console.log(`Added ${msg} to Database.`)
+      });
+    }
+    // Add message (username included) to DB
+    const addMessage = data => {
+      collection.insertOne({user:data.user,message:data.msg}, ()=>{
+        console.log(`Added ${data.user}:${data.msg} to Database.`)
+      });
+    }
 
-  // Handle chat message sending
-  socket.on('new message', data => {
-    // Tell all clients to add message.
-    io.emit('get message', data);
+    // Socket functions
+    let id = socket.id;
+    console.log(`(ID:${id}) User connected...`)
+
+    // Broadcasting new user entering chat
+    socket.on('new user',async user=>{
+      // Render messages from DB on client chat
+      let data = await collection.find({}).toArray();
+      socket.emit('initial render', data);
+
+      // Announce new user to other users
+      username=user;
+      io.emit('announce user',username);
+      addAlert(`${user} has joined the chat!`);
+    });
+
+    // Handle user disconnect
+    socket.on('disconnect', () =>{ 
+      if(username !== undefined){
+        console.log(`(ID:${id}) User disconnected...`)
+        io.emit('user disconnected',username);
+        addAlert(`${username} has left the chat.`);
+      }
+    });
+
+    // Handle chat message sending
+    socket.on('new message', data => {         
+      // Tell all clients to add message.
+      io.emit('get message', data);
+      addMessage(data);
+    });
   });
 });
-
