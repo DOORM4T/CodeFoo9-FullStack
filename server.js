64530d51d1399ca171f5d9ed6d49199580231a2d
Matt Seto <46092255+DOORM4T@users.noreply.github.com>
2019-03-15 20:01:48 -0700

    Add  regex to filter out html tags in chat.

--- server.js
+++ server.js
@@ -7,30 +7,31 @@ app.use(express.static(`${__dirname}/public`));
 let port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
 
-
-// Usernames
-
 // Initialize Socket.io
 const io = require('socket.io')(server);
+let username;
 
 // On client connect
 io.on('connection', (socket) => {
   let id = socket.id;
   console.log(`(ID:${id}) User connected...`)
 
+  // Broadcasting new user entering chat
+  socket.on('new user',user=>{
+    username=user;
+    io.emit('announce user',username);
+  });
 
   // Handle user disconnect
-  socket.on('disconnect', () => console.log(`(ID:${id}) User disconnected...`));
+  socket.on('disconnect', () =>{ 
+    console.log(`(ID:${id}) User disconnected...`)
+    io.emit('user disconnected',username);
+  });
 
   // Handle chat message sending
   socket.on('new message', data => {
     // Tell all clients to add message.
     io.emit('get message', data);
-  });
-  
-  // Broadcasting new user entering chat
-  socket.on('new user',user=>{
-    io.emit('announce user',user);
   });
 });
 
